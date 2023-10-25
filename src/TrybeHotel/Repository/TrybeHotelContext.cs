67dd3dd8b1e47301806aa92df71543ae719ce851
rrodrigodlima <rdrigo019@gmail.com>
2023-10-25 00:50:47 -0300

    req 01

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -4,6 +4,13 @@ using TrybeHotel.Models;
 namespace TrybeHotel.Repository;
 public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
-    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
-    public TrybeHotelContext() { }
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Configurar a conexão com o banco de dados local
+        var connectionString = "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
 }
