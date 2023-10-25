bea9228090d24d82396dde9e2e23b3d49321bd62
rrodrigodlima <rdrigo019@gmail.com>
2023-10-25 02:18:40 -0300

    req06 e 07

--- src/TrybeHotel/Controllers/RoomController.cs
+++ src/TrybeHotel/Controllers/RoomController.cs
@@ -16,21 +16,33 @@ namespace TrybeHotel.Controllers
 
         // 6. Desenvolva o endpoint GET /room/:hotelId
         [HttpGet("{HotelId}")]
-        public IActionResult GetRoom(int HotelId){
-            throw new NotImplementedException();
+        public IActionResult GetRoom(int HotelId)
+        {
+            // Chama o método GetRooms() do repositório para obter a lista de quartos
+            var rooms = _repository.GetRooms(HotelId);
+
+            return Ok(rooms);
         }
 
         // 7. Desenvolva o endpoint POST /room
         [HttpPost]
-        public IActionResult PostRoom([FromBody] Room room){
-            throw new NotImplementedException();
+        public IActionResult PostRoom([FromBody] Room room)
+        {
+
+            // Chame o método AddRoom() do repositório para inserir o quarto
+            var addedRoom = _repository.AddRoom(room);
+
+            return CreatedAtAction(nameof(GetRoom), addedRoom);
         }
 
         // 8. Desenvolva o endpoint DELETE /room/:roomId
         [HttpDelete("{RoomId}")]
         public IActionResult Delete(int RoomId)
         {
-            throw new NotImplementedException();
+            // Chame o método DeleteRoom() do repositório para excluir o quarto
+            _repository.DeleteRoom(RoomId);
+
+            return NoContent();
         }
     }
 }
--- src/TrybeHotel/Repository/RoomRepository.cs
+++ src/TrybeHotel/Repository/RoomRepository.cs
@@ -14,17 +14,66 @@ namespace TrybeHotel.Repository
         // 6. Desenvolva o endpoint GET /room/:hotelId
         public IEnumerable<RoomDto> GetRooms(int HotelId)
         {
-            throw new NotImplementedException(); 
+            var rooms = _context.Rooms
+                         .Where(r => r.HotelId == HotelId)
+                         .Select(room => new RoomDto
+                         {
+                             RoomId = room.RoomId,
+                             Name = room.Name,
+                             Capacity = room.Capacity,
+                             Image = room.Image,
+                             Hotel = new HotelDto
+                             {
+                                 HotelId = HotelId,
+                                 Name = room.Hotel.Name,
+                                 Address = room.Hotel.Address,
+                                 CityId = room.Hotel.CityId,
+                                 CityName = room.Hotel.City.Name
+                             }
+                         })
+                         .ToList();
+
+            return rooms;
         }
 
         // 7. Desenvolva o endpoint POST /room
-        public RoomDto AddRoom(Room room) {
-            throw new NotImplementedException(); 
+        public RoomDto AddRoom(Room room)
+        {
+            // Adiciona o novo quarto ao contexto
+            _context.Rooms.Add(room);
+            _context.SaveChanges();
+
+            // Consulta o hotel relacionado para obter as informações do hotel
+            var hotel = _context.Hotels.First(h => h.HotelId == room.HotelId);
+
+            // Mapeia o novo quarto e o hotel relacionado para o formato RoomDto e retorna
+            var addedRoomDto = new RoomDto
+            {
+                RoomId = room.RoomId,
+                Name = room.Name,
+                Capacity = room.Capacity,
+                Image = room.Image,
+                Hotel = new HotelDto
+                {
+                    HotelId = hotel.HotelId,
+                    Name = hotel.Name,
+                    Address = hotel.Address,
+                    CityId = hotel.CityId,
+                    CityName = hotel.City.Name
+                }
+            };
+
+            return addedRoomDto;
         }
 
         // 8. Desenvolva o endpoint DELETE /room/:roomId
-        public void DeleteRoom(int RoomId) {
-            throw new NotImplementedException();
+        public void DeleteRoom(int RoomId)
+        {
+            var room = _context.Rooms.FirstOrDefault(r => r.RoomId == RoomId); if (room != null)
+            {
+                _context.Rooms.Remove(room);
+                _context.SaveChanges();
+            }
         }
     }
 }
